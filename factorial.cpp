7a883930c453580ef1b53b7a08d5fb2c6eba598a
Pratham Gupta <prathamgupta1455@gmail.com>
2023-08-20 14:51:37 +0530

    Changed factorial.cpp to handle negitive n

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,10 @@
 #include "functions.h"
+#include <iostream>
 
 int factorial(int n){
+    if(n<1) {
+        std::cout<<"Incorrect Input \n Please re enter the number \n";
+    }
     if(n!=1){
 	return(n * factorial(n-1));
     }
