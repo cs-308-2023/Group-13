5f807aaf08023bfcc8c930b75b11c2e4303b831b
Pratham Gupta <prathamgupta1455@gmail.com>
2023-08-20 14:57:12 +0530

    Code refactored to improve readability

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,15 @@
 #include "functions.h"
 #include <iostream>
 
-int factorial(int n){
-    if(n<1) {
-        std::cout<<"Incorrect Input \n Please re enter the number \n";
+int factorial(int n)
+{
+  if(n<1) 
+    {
+      std::cout<<"Incorrect Input \n Please re enter the number \n";
     }
-    if(n!=1){
-	return(n * factorial(n-1));
+  if(n!=1)
+    {
+return(n * factorial(n-1));
     }
-    else return 1;
+  else return 1;
 }
