ada0d86ec25380cc6fad95f12b29244632122bb7
Pratham Gupta <92988197+activelearner7@users.noreply.github.com>
2023-08-27 11:26:29 +0530

    Function call to find GCD added

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ int main(){
     print_hello();
     cout<<endl;
     cout<<"The factorial of 5 is"<<factorial(5)<<endl;
+    
+    int x = 56;
+    int y = 98;
+    cout<<"The gcd of 56 and 98 is"<<findgcd(x, y)<<endl;
     return 0;
 }
